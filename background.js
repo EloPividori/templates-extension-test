043c246af24b6ac7f172c0f00d9a83519f160e5d
Eloise Pividori <eloisepividori@gmail.com>
2023-03-24 11:48:38 +0100

    Got the extension working

--- background.js
+++ background.js
@@ -2,10 +2,24 @@ let authToken
 let categories = []
 const baseURL = 'http://localhost:3000/'
 
+const displayLogin = () => {
+
+  const html = `<div id="template-extension-popup" style="background: rgba(0,0,0,0.3); position: fixed; top: 0; left: 0; width: 100%; height: 100%; display: flex; align-items: center; justify-content: center;">
+    <div style="background: white; border-radius: 20px; padding: 30px; max-width: 500px; width: 100%;">
+    <a href='http://localhost:3000//users/sign_in' target="blank">Sign in</a>
+    </div>
+  </div>`
+  document.body.insertAdjacentHTML('beforeend', html)
+}
 
 const displayCategories = (categories) => {
   const buildCategory = (category) => {
-    return `<div style="border: 1px solid black; padding: 5px; margin-bottom: 5px;">${category.title}</div>`
+    let categoriesHtml = `<div style="border: 1px solid black; padding: 5px; margin-bottom: 5px;">${category.title}`
+    let templatesHtml = ''
+    category.templates.forEach(template => {
+      templatesHtml += `<div class="template" style="border: 1px solid black; padding: 5px; margin-bottom: 5px;">${template["title"]}</div>`
+    })
+    return (categoriesHtml + templatesHtml + '</div>')
   }
 
   const buildCategories = (cats) => {
@@ -16,19 +30,24 @@ const displayCategories = (categories) => {
     return catsHTML
   }
 
-  const random = () => {
-    console.log("YOOO")
-  }
 
-  const html = `<div id="template-extension-popup" style="background: rgba(0,0,0,0.3); position: fixed; top: 0; left: 0; width: 100%; height: 100%; display: flex; align-items: center; justify-content: center;">
-    <div style="background: white; border-radius: 20px; padding: 30px; max-width: 500px; width: 100%;">
-      <h2>My Categories</h2>
-      ${buildCategories(categories)}
+  const html = `<div id="template-extension-popup" style="z-index:9999;background: rgba(0,0,0,0.3); position: fixed; top: 0; left: 0; width: 100%; height: 100%; display: flex; align-items: center; justify-content: center;">
+  <div style="background: white; border-radius: 20px; padding: 30px; max-width: 500px; max-height: 50%; width: 100%;overflow-y:scroll;">
+  <h2>My Categories</h2>
+  ${buildCategories(categories)}
       <button id="populate-template">fill template</button>
       <div id="template-content"></div>
     </div>
   </div>`
   document.body.insertAdjacentHTML('beforeend', html)
+  document.querySelectorAll(".template").forEach((templateCard) => {
+    templateCard.addEventListener('click', (event) => {
+
+      // add to clipboard
+      navigator.clipboard.writeText(event.currentTarget.textContent)
+      document.querySelector('#template-extension-popup').remove()
+    })
+  })
 
   const popButton = document.querySelector('#populate-template')
   popButton.addEventListener('click', (event) => {
@@ -72,10 +91,19 @@ chrome.cookies.get(
 
 chrome.action.onClicked.addListener((tab) => {
   if (!tab.url.includes('chrome://')) {
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: displayCategories,
-      args: [ categories ],
-    });
+    chrome.cookies.get({url: baseURL, name: 'auth_token'}, (cookie) => {
+      if (cookie.value !== '') {
+        chrome.scripting.executeScript({
+          target: { tabId: tab.id },
+          function: displayCategories,
+          args: [ categories ],
+        });
+      } else {
+        chrome.scripting.executeScript({
+          target: { tabId: tab.id },
+          function: displayLogin,
+        })
+      }
+  })
   }
 });
